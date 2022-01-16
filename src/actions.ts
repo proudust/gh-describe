6b2616a775ea578d7d0f281527503e40f7046eba
Proudust <proudust@gmail.com>
2022-01-16 19:08:17 +0900

    :sparkles: Added build script for gh-actions

--- src/actions.ts
+++ src/actions.ts
@@ -1,22 +1,28 @@
 import { debug, getInput, info, setFailed, setOutput } from "https://esm.sh/@actions/core@1.6.0";
 import { ghDescribe } from "./mod.ts";
 
-const repo = getInput("repo", { required: true });
-debug(`input repo: ${repo}`);
-const commitish = getInput("commit-ish", { required: true });
-debug(`input commit-ish: ${commitish}`);
-const defaultValue = getInput("default");
-debug(`input default: ${defaultValue}`);
+async function run() {
+  const token = getInput("token", { required: true });
+  const repo = getInput("repo", { required: true });
+  debug(`input repo: ${repo}`);
+  const commitish = getInput("commit-ish", { required: true });
+  debug(`input commit-ish: ${commitish}`);
+  const defaultValue = getInput("default");
+  debug(`input default: ${defaultValue}`);
 
-try {
-  const { describe, tag, distance, sha } = await ghDescribe(repo, commitish, defaultValue);
+  try {
+    Deno.env.set("GITHUB_TOKEN", token);
+    const { describe, tag, distance, sha } = await ghDescribe(repo, commitish, defaultValue);
 
-  info(describe);
-  setOutput("describe", describe);
-  setOutput("tag", tag);
-  setOutput("distance", distance);
-  setOutput("sha", sha);
-} catch (e: unknown) {
-  const message = (e instanceof Error && e.stack) || String(e);
-  setFailed(message);
+    info(describe);
+    setOutput("describe", describe);
+    setOutput("tag", tag);
+    setOutput("distance", distance);
+    setOutput("sha", sha);
+  } catch (e: unknown) {
+    const message = (e instanceof Error && e.stack) || String(e);
+    setFailed(message);
+  }
 }
+
+run();
---
+++ src/actions_build.ts
@@ -0,0 +1,33 @@
+import { build as dnt } from "https://deno.land/x/dnt@0.16.0/mod.ts";
+import { build as esbuild } from "https://deno.land/x/esbuild@v0.14.11/mod.js";
+import Package from "../package.json" assert { type: "json" };
+
+console.log("$ dnt");
+await dnt({
+  entryPoints: ["./src/actions.ts"],
+  outDir: "./dist/dnt",
+  shims: {
+    deno: true,
+  },
+  test: false,
+  declaration: false,
+  cjs: false,
+  package: {
+    name: Package.name,
+    version: Package.version,
+  },
+});
+
+console.log("$ esbuild");
+await esbuild({
+  bundle: true,
+  entryPoints: ["dist/dnt/esm/actions.js"],
+  outfile: "./dist/index.js",
+  platform: "node",
+  target: "es2021",
+});
+
+console.log("$ rm -rf ./dist/dnt");
+await Deno.remove("./dist/dnt", { recursive: true });
+
+Deno.exit();
