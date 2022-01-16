7b3f774fc05777c69fd8ce09385b75be2e1108e9
Proudust <proudust@gmail.com>
2022-01-16 20:16:40 +0900

    :sparkles: Impl get current repository owner and name

--- src/cli.ts
+++ src/cli.ts
@@ -1,7 +1,71 @@
 import { Command } from "https://deno.land/x/cliffy@v0.20.1/command/mod.ts";
 import { ghDescribe } from "./mod.ts";
 
-type CommandOptions = { repo: string; default: string };
+interface Remote {
+  name: string;
+  fetchUrl?: string;
+  pushUrl?: string;
+}
+
+async function listRemotes() {
+  const process = Deno.run({
+    cmd: ["git", "remote", "-v"],
+    stdout: "piped",
+    stderr: "piped",
+  });
+  const [status, stdout, stderr] = await Promise.all([
+    process.status(),
+    process.output(),
+    process.stderrOutput(),
+  ]);
+  if (status.code === 0) {
+    const lines = (new TextDecoder().decode(stdout))
+      .trim()
+      .split("\n")
+      .map<string[]>((x) => /(.+)\s+(.+)\s+\((push|fetch)\)/.exec(x) || [])
+      .filter((x) => x.length === 4);
+    const remotes: Remote[] = [];
+    let name: string | undefined = undefined;
+    let fetchUrl: string | undefined = undefined;
+    let pushUrl: string | undefined = undefined;
+    for (const [_, lineName, lineUrl, type] of lines) {
+      if (!name) {
+        name = lineName;
+      } else if (name != lineName) {
+        remotes.push({ name, fetchUrl, pushUrl });
+        name = lineName;
+        fetchUrl = pushUrl = undefined;
+      }
+
+      switch (type) {
+        case "fetch":
+          fetchUrl = lineUrl;
+          break;
+        case "push":
+          pushUrl = lineUrl;
+          break;
+      }
+    }
+
+    if (name) {
+      remotes.push({ name, fetchUrl, pushUrl });
+    }
+    return remotes;
+  } else {
+    throw new Error((new TextDecoder().decode(stderr)).trim());
+  }
+}
+
+async function getOriginRepo() {
+  const remotes = await listRemotes();
+  const { fetchUrl } = remotes.find((x) => x.name === "origin" && x.fetchUrl) || remotes[0];
+  if (!fetchUrl) throw new Error();
+
+  const [_, owner, name] = new URL(fetchUrl).pathname.split("/", 3);
+  return `${owner}/${name.endsWith(".git") ? name.substring(0, name.length - 4) : name}`;
+}
+
+type CommandOptions = { repo?: string; default?: string };
 type CommandArguments = [commitIsh: string | undefined];
 
 await new Command<CommandOptions, CommandArguments>()
@@ -12,13 +76,15 @@ await new Command<CommandOptions, CommandArguments>()
   .option("--default", "It is output instead when it action fails. If empty, this step will fail.")
   .arguments("[commit-ish]")
   .action(async (options, commitish) => {
-    await Deno.permissions.request({ name: "run", command: "gh" });
-    console.debug(options);
-
-    const { repo, default: defaultValue } = options;
+    const repo = options.repo || await (async () => {
+      await Deno.permissions.request({ name: "run", command: "git" });
+      return await getOriginRepo();
+    })();
+    const defaultValue = options.default;
     commitish ||= "master";
 
     try {
+      await Deno.permissions.request({ name: "run", command: "gh" });
       const { describe } = await ghDescribe(repo, commitish, defaultValue);
       console.log(describe);
     } catch (e: unknown) {
