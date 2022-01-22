7bab239c8d2371c9fca8faaecc75c93b4131e598
Proudust <proudust@gmail.com>
2022-01-22 18:42:02 +0900

    :sparkles: Add deno compile

--- _build.ts
+++ _build.ts
@@ -1,3 +1,4 @@
+import { join } from "https://deno.land/std@0.122.0/path/mod.ts";
 import { build as dnt } from "https://deno.land/x/dnt@0.16.1/mod.ts";
 import { build as esbuild } from "https://deno.land/x/esbuild@v0.14.11/mod.js";
 import Package from "./package.json" assert { type: "json" };
@@ -39,5 +40,34 @@ await Promise.all([
     target: "es2021",
   }),
 ]);
+
+for (
+  const target of [
+    "x86_64-unknown-linux-gnu",
+    "x86_64-pc-windows-msvc",
+    "x86_64-apple-darwin",
+    "aarch64-apple-darwin",
+  ]
+) {
+  console.log(`$ deno compile --target ${target}`);
+  const cmd = [
+    "deno",
+    "compile",
+    "-q",
+    "--allow-run",
+    "--no-check",
+    "-o",
+    join("dist", target, "gh-describe"),
+    "--target",
+    target,
+    "cli/main.ts",
+  ];
+  const process = Deno.run({ cmd });
+  const { code } = await process.status();
+  if (code !== 0) {
+    const command = cmd.map((x) => x.includes(" ") ? `"${x}"` : x).join(" ");
+    throw new Error(`\`${command}\` exit code is not zero, ExitCode: ${code}`);
+  }
+}
 
 Deno.exit();
