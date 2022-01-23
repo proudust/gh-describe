d4a970c784e64ab9c991a9e2dff5cd74aefa58e3
Proudust <proudust@gmail.com>
2022-01-23 13:16:49 +0900

    :wrench: Split workflow job

--- _build.ts
+++ _build.ts
@@ -1,4 +1,3 @@
-import { join } from "https://deno.land/std@0.122.0/path/mod.ts";
 import { build as dnt } from "https://deno.land/x/dnt@0.16.1/mod.ts";
 import { build as esbuild } from "https://deno.land/x/esbuild@v0.14.13/mod.js";
 import { ghDescribe } from "./core/mod.ts";
@@ -50,46 +49,6 @@ await Promise.all([
       "import.meta.url": "undefined",
     },
   }),
-  await esbuild({
-    bundle: true,
-    entryPoints: ["./cli/main.ts"],
-    outfile: "./dist/cli_deno.js",
-    format: "esm",
-    target: "es2021",
-    define: {
-      "globalThis.version": `"${describe}"`,
-    },
-  }),
 ]);
-
-for (
-  const target of [
-    "x86_64-unknown-linux-gnu",
-    "x86_64-pc-windows-msvc",
-    "x86_64-apple-darwin",
-    "aarch64-apple-darwin",
-  ]
-) {
-  console.log(`$ deno compile --target ${target}`);
-  const cmd = [
-    "deno",
-    "compile",
-    "-q",
-    "--allow-run",
-    "--unstable",
-    "--no-check",
-    "-o",
-    join("dist", `gh-describe-${target}`),
-    "--target",
-    target,
-    "./dist/cli_deno.js",
-  ];
-  const process = Deno.run({ cmd });
-  const { code } = await process.status();
-  if (code !== 0) {
-    const command = cmd.map((x) => x.includes(" ") ? `"${x}"` : x).join(" ");
-    throw new Error(`\`${command}\` exit code is not zero, ExitCode: ${code}`);
-  }
-}
 
 Deno.exit();
---
+++ _compile.ts
@@ -0,0 +1,50 @@
+import { join } from "https://deno.land/std@0.122.0/path/mod.ts";
+import { build as esbuild } from "https://deno.land/x/esbuild@v0.14.13/mod.js";
+import { ghDescribe } from "./core/mod.ts";
+
+console.log("$ gh describe");
+const { describe } = await ghDescribe();
+
+console.log("$ esbuild");
+await esbuild({
+  bundle: true,
+  entryPoints: ["./cli/main.ts"],
+  outfile: "./dist/cli_deno.js",
+  format: "esm",
+  target: "es2021",
+  define: {
+    "globalThis.version": `"${describe}"`,
+  },
+});
+
+for (
+  const target of [
+    "x86_64-unknown-linux-gnu",
+    "x86_64-pc-windows-msvc",
+    "x86_64-apple-darwin",
+    "aarch64-apple-darwin",
+  ]
+) {
+  console.log(`$ deno compile --target ${target}`);
+  const cmd = [
+    "deno",
+    "compile",
+    "-q",
+    "--allow-run",
+    "--unstable",
+    "--no-check",
+    "-o",
+    join("dist", `gh-describe-${target}`),
+    "--target",
+    target,
+    "./dist/cli_deno.js",
+  ];
+  const process = Deno.run({ cmd });
+  const { code } = await process.status();
+  if (code !== 0) {
+    const command = cmd.map((x) => x.includes(" ") ? `"${x}"` : x).join(" ");
+    throw new Error(`\`${command}\` exit code is not zero, ExitCode: ${code}`);
+  }
+}
+
+Deno.exit();
