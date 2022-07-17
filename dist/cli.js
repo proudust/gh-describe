652adb363ddbce616263bc75fe5aa8e91a966035
Proudust <proudust@gmail.com>
2022-07-17 10:12:41 +0900

    :rotating_light: Rebuild

--- dist/cli.js
+++ dist/cli.js
@@ -3951,9 +3951,13 @@ var require_dist = __commonJS({
     var __createBinding = exports && exports.__createBinding || (Object.create ? function(o, m, k, k2) {
       if (k2 === void 0)
         k2 = k;
-      Object.defineProperty(o, k2, { enumerable: true, get: function() {
-        return m[k];
-      } });
+      var desc = Object.getOwnPropertyDescriptor(m, k);
+      if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+        desc = { enumerable: true, get: function() {
+          return m[k];
+        } };
+      }
+      Object.defineProperty(o, k2, desc);
     } : function(o, m, k, k2) {
       if (k2 === void 0)
         k2 = k;
