1a19d53216274fec4feec2367ae6950cec4b043e
Proudust <proudust@gmail.com>
2022-07-17 11:56:20 +0900

    :arrow_up: Bump @actions/core from 1.8.0 to 1.9.0

--- actions/main.ts
+++ actions/main.ts
@@ -1,4 +1,4 @@
-import { debug, getInput, info, setFailed, setOutput } from "https://esm.sh/@actions/core@1.8.0";
+import { debug, getInput, info, setFailed, setOutput } from "https://esm.sh/@actions/core@1.9.0";
 import { ghDescribe, GhDescribeError } from "../core/mod.ts";
 
 async function run() {
